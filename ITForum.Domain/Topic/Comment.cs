d1e018a68de68a456d1650d5cd8175818765dc11
Ivan Marchenko <vanmaru.kun@gmail.com>
2022-09-04 19:47:50 +0300

    Added Comment class

---
+++ ITForum.Domain/Topic/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITForum.Domain.Topic
+{
+    internal class Comment
+    {
+        public int Id { get; set; }
+        public int AuthorID { get; set; }
+        public string Content { get; set; }
+        public Topic? Topic { get; set; }
+        public Comment? Comm { get; set; }
+    }
+}
