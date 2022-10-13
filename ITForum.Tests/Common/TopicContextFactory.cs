750cce39d30883839acd4516267868049fa57981
Stanislav-PC <stasholoborodyi@gmail.com>
2022-10-13 22:28:58 +0300

    fix DeleteCommentCommandHandler_Success and UpdateCommentCommandHandler_Success

--- ITForum.Tests/Common/TopicContextFactory.cs
+++ ITForum.Tests/Common/TopicContextFactory.cs
@@ -17,6 +17,9 @@ namespace ITForum.Tests.Common
         public static Guid TopicIdForDelete = Guid.NewGuid();
         public static Guid TopicIdForUpdate = Guid.NewGuid();
         public static Guid TopicIdForComment = Guid.NewGuid();
+
+        public static Guid CommIdForDelete = Guid.NewGuid();
+        public static Guid CommIdForUpdate = Guid.NewGuid();
         public static ItForumDbContext Create()
         {
             var options = new DbContextOptionsBuilder<ItForumDbContext>()
@@ -77,6 +80,54 @@ namespace ITForum.Tests.Common
                     Attachments = { },
                     Comment = { },
                     Created = DateTime.Now
+                }
+            );
+            context.SaveChanges();
+            return AddComments(context);
+        }
+        private static ItForumDbContext AddComments(ItForumDbContext context)
+        {
+        context.Database.EnsureCreated();
+            context.Topics.AddRange(
+                new Topic
+                {
+                    Id = Guid.Parse("278867CB-61DD-456E-A9EA-CE53B9A0D191"),
+                    UserId = UserAId,
+                    Name = "Name1",
+                    Content = "Context1",
+                    Attachments = { },
+                    Comment = { },
+                    Created = DateTime.Now
+                });
+            context.Comments.AddRange(
+                new Comment
+                {
+                    Id = Guid.Parse("A6BB65BB-5AC2-4AFA-8A28-2616F675B825"),
+                    UserId = UserAId,
+                    Content = "Context1",
+                    TopicId = Guid.Parse("A6BB65BB-5AC2-4AFA-8A28-2616F675B825"),
+                },
+                new Comment
+                {
+                    Id = Guid.Parse("909F7C29-891B-4BE1-8504-21F84F262084"),
+                    UserId = UserBId,
+                    Content = "Content2",
+                    TopicId = Guid.Parse("A6BB65BB-5AC2-4AFA-8A28-2616F675B825")
+                },
+                new Comment
+                {
+                    UserId = UserAId,
+                    Id = CommIdForDelete,
+                    Content = "Content3",
+                    CommId = Guid.Parse("A6BB65BB-5AC2-4AFA-8A28-2616F675B825")
+                },
+                new Comment
+                {
+
+                    UserId = UserBId,
+                    Id = CommIdForUpdate,
+                    Content = "Content4",
+                    CommId = Guid.Parse("909F7C29-891B-4BE1-8504-21F84F262084")
                 }
             );
             context.SaveChanges();
