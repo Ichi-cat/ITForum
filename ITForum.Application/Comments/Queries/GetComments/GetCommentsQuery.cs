cb95be9837e7882b8bdf056a3286de3e7a6ddd43
Ivan Marchenko <vanmaru.kun@gmail.com>
2022-10-13 22:38:21 +0300

    Added query for getting commands from the database

---
+++ ITForum.Application/Comments/Queries/GetComments/GetCommentsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITForum.Application.Comments.Queries.GetComments
+{
+    public class GetCommentsQuery:IRequest<CommentListVM>
+    {
+        public Guid TopicId { get; set; }
+        public int Count { get; set; }
+    }
+}
--- ITForum.Domain/TopicItems/Comment.cs
+++ ITForum.Domain/TopicItems/Comment.cs
@@ -5,9 +5,7 @@
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public string Content { get; set; }
-        public Topic? Topic { get; set; }
         public Guid? TopicId { get; set; }
-        public Comment? Comm { get; set; }    //оно точно надо? Есть же id
         public Guid? CommId { get; set; }
         public List<Mark> Marks { get; set; }
     }
