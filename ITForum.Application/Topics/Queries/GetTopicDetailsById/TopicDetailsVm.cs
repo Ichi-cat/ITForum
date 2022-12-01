88fa4492993ea791997d2a0d2614f2de496fc9e0
Stanislav-PC <stasholoborodyi@gmail.com>
2022-12-01 17:49:58 +0200

    fix

--- ITForum.Application/Topics/Queries/GetTopicDetailsById/TopicDetailsVm.cs
+++ ITForum.Application/Topics/Queries/GetTopicDetailsById/TopicDetailsVm.cs
@@ -8,10 +8,12 @@ namespace ITForum.Application.Topics.Queries.GetTopicDetailsById
     public class TopicDetailsVm : IMap
     {
         public string Name { get; set; }
+        public Guid UserId { get; set; }
         public string Content { get; set; }
         public List<Attachment> Attachments { get; set; } // TODO: Запилить вывод через attachmentVm
         public int LikeCount { get; set; }
         public int DislikeCount { get; set; }
+        public List<Tag> Tags { get; set; }
 
         public void Mapping(Profile profile)
         {
